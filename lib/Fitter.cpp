813dc476c99a27b414c55f9cab8f26eed545ea0a
Richard Lane <richard.lane@bristol.ac.uk>
2020-04-08 15:20:44 +0100

    implementation for PhysicalFitter (nearly)

--- lib/Fitter.cpp
+++ lib/Fitter.cpp
@@ -428,12 +428,127 @@ void MinuitPolyScan::twoDParamScan(const size_t i,
     }
 }
 
-Fitter::Fitter(const FitData_t& fitData) : BaseFitter(fitData)
+PhysicalFitter::PhysicalFitter(const FitData_t& fitData) : BaseFitter(fitData)
 {
     ;
 }
 
-TMatrixD Fitter::covarianceVector2CorrelationMatrix(const std::vector<double>& covarianceVector)
+void PhysicalFitter::fit(const std::vector<double>& initialParams,
+                         const std::vector<double>& initialErrors,
+                         const FitAlgorithm_t&      FitMethod,
+                         const std::vector<size_t>  fixParams)
+{
+    std::cout << "This isnt implemented properly yet" << std::endl;
+    throw D2K3PiException();
+
+    // Check that we have been passed 6 initial parameters and errors
+    if (initialParams.size() != 6 || initialErrors.size() != 6) {
+        std::cout << "detailedFitUsingMinuit2ChiSq requires a guess of 6 parameters and their errors" << std::endl;
+        throw D2K3PiException();
+    }
+
+    // Create an object representing our Minuit2-compatible 2nd order polynomial
+    _fitFcn = std::make_unique<DetailedPolynomialChiSqFcn>(_fitData.data, _fitData.binCentres, _fitData.errors);
+
+    // Store our parameters
+    ROOT::Minuit2::MnUserParameters parameters;
+    parameters.Add("x", initialParams[0], initialErrors[0]);
+    parameters.Add("y", initialParams[1], initialErrors[1]);
+    parameters.Add("r", initialParams[2], initialErrors[2]);
+    parameters.Add("z_im", initialParams[3], initialErrors[3]);
+    parameters.Add("z_re", initialParams[4], initialErrors[4]);
+    parameters.Add("width", initialParams[5], initialErrors[5]);
+
+    // Create a minimiser and minimise our chi squared
+    ROOT::Minuit2::MnMigrad migrad(*_fitFcn, parameters);
+    migrad.Fix(5); // Fix width
+    ROOT::Minuit2::FunctionMinimum min = migrad();
+
+    // Check that our solution is "valid"
+    // I think this checks that the call limit wasn't reached and that the fit converged, though it's never possible to
+    // be sure with Minuit2
+    if (!min.IsValid()) {
+        std::cerr << "Minuit fit invalid" << std::endl;
+        std::cerr << min << std::endl;
+        throw D2K3PiException();
+    }
+
+    // Store our fit parameters and correlation matrix as class attributes
+    // Set our fitParams to the values obtained in the fit
+    fitParams.fitParams      = min.UserParameters().Params();
+    fitParams.fitParamErrors = min.UserParameters().Errors();
+
+    // Acquire a vector representing the covariance matrix and convert it to a correlation TMatrixD
+    // Hack: remove width from fit params, store matrix and add it again
+    double widthErr = fitParams.fitParamErrors[5];
+    fitParams.fitParamErrors.pop_back();
+    fitParams.correlationMatrix =
+        std::make_unique<TMatrixD>(covarianceVector2CorrelationMatrix(min.UserCovariance().Data()));
+    fitParams.fitParamErrors.push_back(widthErr);
+
+    // Store chi squared
+    statistic = std::make_unique<double>(min.Fval());
+
+    // Set our TGraph pointer to the right thing
+    plot = std::make_unique<TGraphErrors>(_fitData.numPoints,
+                                          _fitData.binCentres.data(),
+                                          _fitData.data.data(),
+                                          _fitData.binErrors.data(),
+                                          _fitData.errors.data());
+
+    // Create also a best-fit dataset from our parameters and data, plotting this on the same
+    DecayParams_t bestFitParams = DecayParameters{.x     = fitParams.fitParams[0],
+                                                  .y     = fitParams.fitParams[1],
+                                                  .r     = fitParams.fitParams[2],
+                                                  .z_im  = fitParams.fitParams[3],
+                                                  .z_re  = fitParams.fitParams[4],
+                                                  .width = fitParams.fitParams[5]};
+
+    // Should use std::transform
+    std::vector<double> bestFitData(_fitData.binCentres.size());
+    std::vector<double> zeros(_fitData.numPoints, 0.0); // Want errors of 0
+    for (size_t i = 0; i < bestFitData.size(); ++i) {
+        bestFitData[i] = fitPolynomial(bestFitParams, _fitData.binCentres[i]);
+    }
+
+    bestFitPlot = std::make_unique<TGraphErrors>(
+        _fitData.numPoints, _fitData.binCentres.data(), bestFitData.data(), zeros.data(), zeros.data());
+}
+
+void PhysicalFitter::saveFitPlot(const std::string&          plotTitle,
+                                 const std::string&          path,
+                                 const util::LegendParams_t* legendParams)
+{
+    // Check that a fit has been made
+    if (fitParams.fitParams.empty() || plot == nullptr) {
+        std::cerr << "Run the fitter before plotting" << std::endl;
+        throw D2K3PiException();
+    }
+
+    // Check that path doesn't already exist
+    if (boost::filesystem::exists(path)) {
+        std::cerr << path << " already exists" << std::endl;
+        throw D2K3PiException();
+    }
+
+    // Set the plot title; for some reason this is also how to set axis labels?
+    plot->SetTitle((plotTitle + ";time/ns;DCS/CF ratio").c_str());
+
+    // Save our fit to file
+    // If root's builtin was used, _bestFitPlot will not have been assigned
+    if (!legendParams) {
+        std::cerr << "Must specify legend parameters if plotting a minuit-fitted graph" << std::endl;
+        throw D2K3PiException();
+    }
+    bestFitPlot->SetLineColor(kRed);
+    util::saveObjectsToFile<TGraphErrors>(std::vector<TObject*>{plot.get(), bestFitPlot.get()},
+                                          std::vector<std::string>{"AP", "CSAME"},
+                                          std::vector<std::string>{"Data", "Best fit"},
+                                          path,
+                                          *legendParams);
+}
+
+TMatrixD PhysicalFitter::covarianceVector2CorrelationMatrix(const std::vector<double>& covarianceVector)
 {
     // Check that we have the right number of elements in our covariance vector
     size_t numElements       = covarianceVector.size();
@@ -475,6 +590,24 @@ TMatrixD Fitter::covarianceVector2CorrelationMatrix(const std::vector<double>& c
     }
     return CorrMatrix;
 }
+
+void PhysicalFitter::_storeMinuitFitParams(const ROOT::Minuit2::FunctionMinimum& min)
+{
+    // Set our fitParams to the values obtained in the fit
+    fitParams.fitParams      = min.UserParameters().Params();
+    fitParams.fitParamErrors = min.UserParameters().Errors();
+
+    // Acquire a vector representing the covariance matrix and convert it to a correlation TMatrixD
+    fitParams.correlationMatrix =
+        std::make_unique<TMatrixD>(covarianceVector2CorrelationMatrix(min.UserCovariance().Data()));
+}
+
+Fitter::Fitter(const FitData_t& fitData) : BaseFitter(fitData)
+{
+    ;
+}
+
+TMatrixD Fitter::covarianceVector2CorrelationMatrix(const std::vector<double>& covarianceVector) {}
 
 void Fitter::detailedFitUsingMinuit(const std::vector<double>& initialParams,
                                     const std::vector<double>& initialErrors,
@@ -557,13 +690,6 @@ void Fitter::detailedFitUsingMinuit(const std::vector<double>& initialParams,
 void Fitter::_storeMinuitFitParams(const ROOT::Minuit2::FunctionMinimum& min)
 {
 
-    // Set our fitParams to the values obtained in the fit
-    fitParams.fitParams      = min.UserParameters().Params();
-    fitParams.fitParamErrors = min.UserParameters().Errors();
-
-    // Acquire a vector representing the covariance matrix and convert it to a correlation TMatrixD
-    fitParams.correlationMatrix =
-        std::make_unique<TMatrixD>(covarianceVector2CorrelationMatrix(min.UserCovariance().Data()));
 }
 
 void Fitter::chiSqParameterScan(const size_t i, const size_t numPoints, const double low, const double high)
@@ -683,38 +809,6 @@ void Fitter::saveFitPlot(const std::string&          plotTitle,
                          const std::string&          path,
                          const util::LegendParams_t* legendParams)
 {
-    // Check that a fit has been made
-    if (fitParams.fitParams.empty() || plot == nullptr) {
-        std::cerr << "Run the fitter before plotting" << std::endl;
-        throw D2K3PiException();
-    }
-
-    // Check that path doesn't already exist
-    if (boost::filesystem::exists(path)) {
-        std::cerr << path << " already exists" << std::endl;
-        throw D2K3PiException();
-    }
-
-    // Set the plot title; for some reason this is also how to set axis labels?
-    plot->SetTitle((plotTitle + ";time/ns;DCS/CF ratio").c_str());
-
-    // Save our fit to file
-    // If root's builtin was used, _bestFitPlot will not have been assigned
-    if (bestFitPlot != nullptr) {
-        if (!legendParams) {
-            std::cerr << "Must specify legend parameters if plotting a minuit-fitted graph" << std::endl;
-            throw D2K3PiException();
-        }
-        bestFitPlot->SetLineColor(kRed);
-        util::saveObjectsToFile<TGraphErrors>(std::vector<TObject*>{plot.get(), bestFitPlot.get()},
-                                              std::vector<std::string>{"AP", "CSAME"},
-                                              std::vector<std::string>{"Data", "Best fit"},
-                                              path,
-                                              *legendParams);
-
-    } else {
-        util::saveObjectToFile(plot.get(), path, "AP");
-    }
 }
 
 #endif // FITTER_CPP
