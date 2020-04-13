685138782831e0701573da5082b883c4a4a697c7
Richard Lane <richard.lane@bristol.ac.uk>
2020-04-13 13:18:26 +0100

    header for param scanner base class

---
+++ lib/fitter/MinuitScannerBase.h
@@ -0,0 +1,44 @@
+#include <utility>
+#include <vector>
+
+#include "MinuitFitterBase.h"
+
+class MinuitScannerBase : public MinuitFitterBase
+{
+    /*
+     * Scan the ith parameter as defined in fitParams.fitParams
+     *
+     * Cannot have more than 100 points due to some of Minuit's internal limitations.
+     * By default runs a scan from +-2sigma, but can optionally be specified by setting low and high.
+     *
+     * Populates parameterScan
+     */
+    virtual void
+    chiSqParameterScan(const size_t i, const size_t numPoints, const double low = 0., const double high = 0.);
+
+    /*
+     * Scan the i and jth parameters between the specified limits
+     *
+     * Populates twoDParameterScan
+     */
+    virtual void twoDParamScan(const size_t i,
+                               const size_t j,
+                               const size_t iPoints,
+                               const size_t jPoints,
+                               const double iLow,
+                               const double iHigh,
+                               const double jLow,
+                               const double jHigh);
+
+    /*
+     * Vector of pairs describing a parameter scan
+     */
+    std::vector<std::pair<double, double>> parameterScan;
+
+    /*
+     * Vector of tuples describing a 2d parameter scan
+     *
+     * Scans parameters i and j to find chi squared values; result is a vector of (i_value, j_value, chi_squared)
+     */
+    std::vector<std::vector<double>> twoDParameterScan;
+};
