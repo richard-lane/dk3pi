e1aaa57974fcb0460c44074605a3a78d43eb4812
Richard Lane <richard.lane@bristol.ac.uk>
2020-05-27 11:38:23 +0100

    skeleton for ampgenpull C++ file

---
+++ test/ampGenPull/ampgenPull.cpp
@@ -0,0 +1,21 @@
+/*
+ * Read in two ROOT files d.root and dBar.root, take their ratios of decay times
+ */
+void ampgenFit()
+{
+    // Usage: ampgenPull.exe <Dbar file> <D file>
+
+    // Read in ROOT files
+
+    // Take ratios
+
+    // Perform a fit
+
+    // Save the fit parameters and errors to a text file
+}
+
+int main()
+{
+    ampgenFit();
+    return 0;
+}
