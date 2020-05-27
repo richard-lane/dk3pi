2911c3ebdec59bc24ea16d4c158076459aa5aa3c
Richard Lane <richard.lane@bristol.ac.uk>
2020-05-27 11:42:29 +0100

    CLI checking for ampgenpull

--- test/ampGenPull/ampgenPull.cpp
+++ test/ampGenPull/ampgenPull.cpp
@@ -1,10 +1,10 @@
+#include "D2K3PiError.h"
+
 /*
  * Read in two ROOT files d.root and dBar.root, take their ratios of decay times
  */
-void ampgenFit()
+void ampgenFit(const char* dFile, const char* dBarFile)
 {
-    // Usage: ampgenPull.exe <Dbar file> <D file>
-
     // Read in ROOT files
 
     // Take ratios
@@ -14,8 +14,15 @@ void ampgenFit()
     // Save the fit parameters and errors to a text file
 }
 
-int main()
+int main(int argc, char* argv[])
 {
-    ampgenFit();
+    // Should probably also check that D and Dbar have been passed in the right order
+    // but what are the chances of that
+    if (argc != 4) {
+        std::cerr << "Usage: ./ampgenpull <d root file> <d bar root file>" << std::endl;
+        throw D2K3PiException();
+    }
+
+    ampgenFit(argv[1], argv[2]);
     return 0;
 }
