4189ddb4cc5c9bcbb9f60c948476e8ff5ee659dc
Richard Lane <richard.lane@bristol.ac.uk>
2020-05-27 12:14:59 +0100

    calculate ratios from ampgen

--- test/ampGenPull/ampgenPull.cpp
+++ test/ampGenPull/ampgenPull.cpp
@@ -1,5 +1,7 @@
 #include "D2K3PiError.h"
+#include "RatioCalculator.h"
 #include "ReadAmpGen.h"
+#include "util.h"
 
 #include <TFile.h>
 
@@ -10,6 +12,9 @@
  */
 void ampgenFit(const char* dFile, const char* dBarFile)
 {
+    // Data that will be used for fitting/calculating pulls
+    double width = 2438.4;
+
     // Read in ROOT files
     std::unique_ptr<TFile> mixedDecays    = std::make_unique<TFile>(dFile);
     std::unique_ptr<TFile> favouredDecays = std::make_unique<TFile>(dBarFile);
@@ -19,7 +24,14 @@ void ampgenFit(const char* dFile, const char* dBarFile)
     MixedData.populate("D_decayTime");
     FavouredData.populate("Dbar0_decayTime");
 
+    // Time binning
+    std::vector<double> binLimits = util::exponentialBinLimits(0.006, width, 25);
+    std::vector<size_t> cfCounts  = util::binVector(FavouredData.decayTimes, binLimits);
+    std::vector<size_t> dcsCounts = util::binVector(MixedData.decayTimes, binLimits);
+
     // Take ratios
+    RatioCalculator ratios(cfCounts, dcsCounts, binLimits);
+    ratios.calculateRatios();
 
     // Perform a fit
 
