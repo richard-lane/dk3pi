07afe24946dc3019cce76550944d5dc1678871e8
Richard Lane <richard.lane@bristol.ac.uk>
2020-05-27 11:55:31 +0100

    read in root files

--- test/ampGenPull/ampgenPull.cpp
+++ test/ampGenPull/ampgenPull.cpp
@@ -1,4 +1,9 @@
 #include "D2K3PiError.h"
+#include "ReadAmpGen.h"
+
+#include <TFile.h>
+
+#include <memory>
 
 /*
  * Read in two ROOT files d.root and dBar.root, take their ratios of decay times
@@ -6,7 +11,13 @@
 void ampgenFit(const char* dFile, const char* dBarFile)
 {
     // Read in ROOT files
-    
+    std::unique_ptr<TFile> mixedDecays    = std::make_unique<TFile>(dFile);
+    std::unique_ptr<TFile> favouredDecays = std::make_unique<TFile>(dBarFile);
+
+    D2K3PiData MixedData    = D2K3PiData(mixedDecays.get(), "DalitzEventList");
+    D2K3PiData FavouredData = D2K3PiData(favouredDecays.get(), "DalitzEventList");
+    MixedData.populate("D_decayTime");
+    FavouredData.populate("Dbar0_decayTime");
 
     // Take ratios
 
@@ -19,7 +30,7 @@ int main(int argc, char* argv[])
 {
     // Should probably also check that D and Dbar have been passed in the right order
     // but what are the chances of that
-    if (argc != 4) {
+    if (argc != 3) {
         std::cerr << "Usage: ./ampgenpull <d root file> <d bar root file>" << std::endl;
         throw D2K3PiException();
     }
