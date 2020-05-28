5c339984b60d02342e8994a591b706c7a4526cdb
Richard Lane <richard.lane@bristol.ac.uk>
2020-05-28 12:26:39 +0100

    fcn for finding pulls

--- test/ampGenPull/pull.py
+++ test/ampGenPull/pull.py
@@ -8,6 +8,7 @@ import numpy as np
 def num_events(dcs_mean, cf_mean):
     """
     Returns number of DCS and CF events to generate
+    Returns (DCS counts, CF counts)
 
     """
     return np.random.poisson(dcs_mean), np.random.poisson(cf_mean)
@@ -27,14 +28,33 @@ def run_ampgen(options_file, root_file, num_events, event_type):
             ampgen_generator,
             options_file,
             "--nEvents",
-            num_events,
+            str(num_events),
             "--EventType",
             event_type,
             "--Output",
             root_file,
+            "--GenerateTimeDependent",
         ],
         check=True,
     )
+
+
+def find_pull(out_file):
+    """
+    Find pull from data in a text file
+
+    """
+    expected_rd = 0.0549
+    r_pulls = []
+    with open(out_file, "r") as f:
+        fit_results = [line.rstrip() for line in f]
+
+        for result in fit_results:
+            result = result.split(",")
+            r = result[0]
+            dr = result[1]
+            r_pulls.append((float(r) - expected_rd) / float(dr))
+    print(np.mean(r_pulls), "+-", np.std(r_pulls))
 
 
 def cli():
@@ -62,12 +82,10 @@ def cli():
 
 
 def main(args):
-    dcs_fraction = (
-        75
-    )  # Calculated from the phase space params that we expect, given the inputs to AmpGen
-
     # Find how many events to generate from a poisson distribution
-    num_cf, num_dcs = num_events(args.numCF / dcs_fraction, args.numCF)
+    # Calculate relative number of events from phase space params given by AmpGen
+    dcs_fraction = 75
+    num_dcs, num_cf = num_events(int(args.numCF / dcs_fraction), int(args.numCF))
 
     # Run AmpGen
     cf_opt_file = os.path.abspath(
@@ -94,6 +112,9 @@ def main(args):
     run_ampgen(dcs_opt_file, dcs_root_file, num_dcs, dcs_event_type)
     run_ampgen(cf_opt_file, cf_root_file, num_cf, cf_event_type)
 
+    # Create out_file if it doesn't exist
+    open(args.out_file, "w+").close()
+
     # Read ROOT files, calculate ratios, fit + save the results to a file
     fitter_executable = os.path.abspath(
         os.path.join(
@@ -105,19 +126,11 @@ def main(args):
         )
     )
     subprocess.run(
-        [fitter_executable, dcs_root_file, cf_root_file, out_file], check=True
+        [fitter_executable, dcs_root_file, cf_root_file, args.out_file], check=True
     )
 
-    r_pulls = []
-    with open(out_file, "r") as f:
-        fit_results = [line.rstrip() for line in f]
-
-        for result in fit_results:
-            result = result.split(",")
-            r = result[0]
-            dr = result[1]
-            r_pulls.append((float(r) - 0.0549) / float(dr))
-    print(np.mean(r_pulls), "+-", np.std(r_pulls))
+    # Find and print pull for r
+    find_pull(args.out_file)
 
 
 if __name__ == "__main__":
