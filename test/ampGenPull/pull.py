f8ff030eedd6ce738b868c1f24686e462cb2c513
Richard Lane <richard.lane@bristol.ac.uk>
2020-05-28 10:50:32 +0100

    skeleton python ampgenpull

---
+++ test/ampGenPull/pull.py
@@ -0,0 +1,14 @@
+import sys
+import os
+import subprocess
+
+def main():
+    # Find how many events to generate from a poisson distribution
+
+    # Run AmpGen
+
+    # Read ROOT files, calculate ratios, fit + save the results to a file
+    pass
+
+if __name__ == '__main__':
+    main()
