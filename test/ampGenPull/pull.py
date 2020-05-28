2bfc1094c96d3a6575c40649a721ac586a14ecda
Richard Lane <richard.lane@bristol.ac.uk>
2020-05-28 10:57:38 +0100

    pull arg parsing

--- test/ampGenPull/pull.py
+++ test/ampGenPull/pull.py
@@ -1,8 +1,28 @@
 import sys
 import os
 import subprocess
+import argparse
 
-def main():
+
+def cli():
+    """
+    CLI parsing
+
+    Returns an argparse args object
+    """
+    parser = argparse.ArgumentParser(
+        description="Run a pull study using events generated with AmpGen"
+    )
+    parser.add_argument(
+        "--numCF",
+        default=60000,
+        type=int,
+        help="Number of CF events to generate. The number of DCS events is calculated using the phase space parameters used by AmpGen (defined in the relevant .opt files). Defaults to 60000",
+    )
+    return parser.parse_args()
+
+
+def main(args):
     # Find how many events to generate from a poisson distribution
 
     # Run AmpGen
@@ -10,5 +30,6 @@ def main():
     # Read ROOT files, calculate ratios, fit + save the results to a file
     pass
 
-if __name__ == '__main__':
-    main()
+
+if __name__ == "__main__":
+    main(cli())
