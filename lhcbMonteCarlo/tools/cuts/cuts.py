48b5468c550ce189581d91f3255b31dcf09148a4
Richard Lane <richard.lane@bristol.ac.uk>
2020-07-29 14:24:57 +0100

    lazy cuts skeleton file

---
+++ lhcbMonteCarlo/tools/cuts/cuts.py
@@ -0,0 +1,18 @@
+"""
+Script for parsing our LHCb Monte Carlo dataset and just extracting the data we want/need
+
+Implments the cuts in cuts.txt; uses uproot
+
+"""
+import uproot
+
+
+def main():
+    # Locate the branches we actually might want
+    # Perform cuts
+    # Write to a new file
+    pass
+
+
+if __name__ == "__main__":
+    main()
