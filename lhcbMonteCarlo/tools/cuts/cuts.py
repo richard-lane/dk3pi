8eec78b7b006ce25a8d993922570885c5d7b31c1
Richard Lane <richard.lane@bristol.ac.uk>
2020-07-29 17:18:56 +0100

    CLI args

--- lhcbMonteCarlo/tools/cuts/cuts.py
+++ lhcbMonteCarlo/tools/cuts/cuts.py
@@ -4,13 +4,17 @@ Script for parsing our LHCb Monte Carlo dataset and just extracting the data we 
 Implments the cuts in cuts.txt; uses uproot
 
 """
+import argparse
 import uproot4
 
 
-def main():
+def main(args):
     # Locate the branches we actually might want
     # It's possible I have the 'wrong' branches here, but I'm looking for kinematic branches + branches I need to make cuts
-    required_branches = {
+    # Read these branches into a new ROOT file
+    # Perform cuts
+    # Write to a new file
+    default_branches = {
         "Dstar_M",
         "D0_IPCHI2_OWNPV",
         "D0_PE",
@@ -36,19 +40,43 @@ def main():
         "pi3minus_PY",
         "pi3minus_PZ",
     }
-    # Read these branches into a new ROOT file
-    # Perform cuts
-    # Write to a new file
-    pass
+
+    # If we just want to show the default branches, then print and exit
+    if args.show_default_branches:
+        print("Default branches to read:")
+        for branch in default_branches:
+            print("\t", branch)
+        return
 
 
 def cli():
     """
     Argument parsing for this script
 
+    Returns argparse ArgumentParser object
+
     """
-    pass
+    parser = argparse.ArgumentParser(
+        description="Parsing and cutting a ROOT file down to only the required branches. Cuts are hard-coded for now"
+    )
+    required_arguments = parser.add_mutually_exclusive_group(required=True)
+    required_arguments.add_argument("--inFile", help="ROOT file to read")
+    required_arguments.add_argument(
+        "--show-default-branches",
+        action="store_true",
+        help="Print the list of default branches and exit",
+    )
+
+    parser.add_argument("--outFile", help="File to write to", default="out.root")
+
+    parser.add_argument(
+        "--branches",
+        help="Additional branches to read. Any arguments provided are appended to the list of default branches",
+        nargs="*",
+    )
+
+    return parser.parse_args()
 
 
 if __name__ == "__main__":
-    main()
+    main(cli())
