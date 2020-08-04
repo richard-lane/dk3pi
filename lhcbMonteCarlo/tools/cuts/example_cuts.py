06967fa9bda235021e9dde9194e46eb6dbb4734e
Richard Lane <richard.lane@bristol.ac.uk>
2020-08-04 14:05:32 +0100

    cut documentation and description

--- lhcbMonteCarlo/tools/cuts/example_cuts.py
+++ lhcbMonteCarlo/tools/cuts/example_cuts.py
@@ -26,46 +26,26 @@ BRANCHES = [
 ]
 
 
-def cut_delta_m(Dstar_M, D0_M):
-    """
-    Returns bool for our DeltaM cut
-    True if deltam[i] is in the allowed range
-
-    masses in MeV
-
-    """
-    return 139.3 < Dstar_M - D0_M < 152
-
-
-def cut_d_mass(D0_M):
-    """
-    Returns bool for our D mass cut
-    True if d_masses[i] is in the allowed range
-
-    d_masses in MeV
-
-    """
-    return 1840.83 < D0_M < 1888.83
-
-
+# Cuts should start with "cut"
 def cut_d_impact_param(D0_IPCHI2_OWNPV):
-    """
-    Returns bool for impact parameter cut
-    True if our chisq is below the allowed value
-
-    Takes in D0_IPCHI2_OWNPV
-
-    """
     return D0_IPCHI2_OWNPV < 9
 
 
+# They can accept multiple parameters
+def cut_delta_m(Dstar_M, D0_M):
+    return 139.3 < Dstar_M - D0_M < 152
+
+
+# Functions not matching the pattern cut* will be ignored
 def not_a_cut_function(f):
     assert False
 
 
-def uses_helper():
-    """
-    Check if this works
+def d_mass_helper(D0_M):
+    return 1840.83 < D0_M < 1888.83
 
-    """
-    not_a_cut_function(2)
+
+# Functions can call other functions, in case your cut is complicated
+def cut_d_mass(D0_M):
+    return d_mass_helper(2)
+
