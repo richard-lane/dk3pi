844d7e8e03c9b80df798c61c5b0e1ae0a08fdcdd
Richard Lane <richard.lane@bristol.ac.uk>
2020-08-04 11:11:10 +0100

    example cuts file thing

---
+++ lhcbMonteCarlo/tools/cuts/cutParser.py
@@ -0,0 +1,14 @@
+"""
+Script for reading a module of cuts implemented in python and applying them to a dataset
+
+Needs to import a module from the command line, find what cuts it contains and apply them to our dataset
+
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ lhcbMonteCarlo/tools/cuts/example_cuts.py
@@ -0,0 +1,31 @@
+def cut_d_mass(D0_M):
+    """
+    Returns bool for our D mass cut
+    True if d_masses[i] is in the allowed range
+
+    d_masses in MeV
+
+    """
+    return 1840.83 < D0_M < 1888.83
+
+
+def cut_delta_m(Dstar_M, D0_M):
+    """
+    Returns bool for our DeltaM cut
+    True if deltam[i] is in the allowed range
+
+    masses in MeV
+
+    """
+    return 139.3 < Dstar_M - D0_M < 152
+
+
+def cut_d_impact_param(D0_IPCHI2_OWNPV):
+    """
+    Returns bool for impact parameter cut
+    True if our chisq is below the allowed value
+
+    Takes in D0_IPCHI2_OWNPV
+
+    """
+    return D0_IPCHI2_OWNPV < 9
