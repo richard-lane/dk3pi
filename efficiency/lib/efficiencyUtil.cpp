d707db03c0647b630446a411afe0ce979ad0d1a9
Richard Lane <richard.lane@bristol.ac.uk>
2020-08-06 12:16:15 +0100

    implementation for event -> invariant mass calculation

--- efficiency/lib/efficiencyUtil.cpp
+++ efficiency/lib/efficiencyUtil.cpp
@@ -21,6 +21,10 @@ double invariantMass(const std::vector<kinematicParams_t>& systemKinematics)
 
 std::vector<double> event2invariantMasses(const dDecay_t& event)
 {
-    (void)event;
-    return std::vector<double>{1, 2, 3};
+    return std::vector<double>{
+        invariantMass(std::vector<kinematicParams_t>{event.kParams, event.pi1Params}),
+        invariantMass(std::vector<kinematicParams_t>{event.pi1Params, event.pi2Params}),
+        invariantMass(std::vector<kinematicParams_t>{event.pi2Params, event.pi3Params}),
+        invariantMass(std::vector<kinematicParams_t>{event.kParams, event.pi1Params, event.pi2Params}),
+        invariantMass(std::vector<kinematicParams_t>{event.pi1Params, event.pi2Params, event.pi3Params})};
 }
