35c33d4c1a3dab0669519277478ac5b3e9943fcf
Richard Lane <richard.lane@bristol.ac.uk>
2020-09-09 12:32:09 +0100

    i guess make the z scan script a bit nicer

--- charmFitter/scripts/test_param_scan.cpp
+++ charmFitter/scripts/test_param_scan.cpp
@@ -1,5 +1,6 @@
 /* Lots of reused code here */
 #include <iostream>
+#include <memory>
 #include <vector>
 
 #include "DecaySimulator.h"
@@ -52,30 +53,15 @@ void plotScan(MinuitPolynomialFitter& MinuitChiSqFitter,
     util::saveObjectToFile(Graph, (graphTitle + ".pdf").c_str(), "AP");
 }
 
-/*
- * Create a dataset using accept-reject and our RatioCalculator then fit it to polynomials using
- * Minuit ChiSq
- *
- * Perform a scan of each parameter, store them as std::vector<std::pair<double, double>> and plot
- */
-void test_param_scan(void)
+SimulatedDecays generateDecays(const FitterUtil::DecayParams_t& phaseSpaceParams,
+                               const double                     maxTime,
+                               const size_t                     numCfEvents,
+                               const double                     efficiencyTimescale)
 {
-    // Create an accept-reject dataset
-    FitterUtil::DecayParams_t phaseSpaceParams = {
-        .x     = WORLD_AVERAGE_X,
-        .y     = WORLD_AVERAGE_Y,
-        .r     = 0.055,
-        .z_im  = -0.2956,
-        .z_re  = 0.7609,
-        .width = 2439.0,
-    };
-    double maxTime      = 0.004;
-    size_t numCfEvents  = 700000;
     double numDcsEvents = Phys::numDCSDecays(numCfEvents, phaseSpaceParams, maxTime, 1 / phaseSpaceParams.width);
 
-    double efficiencyTimescale = phaseSpaceParams.width;
-    auto   cfRate              = [&](double x) { return Phys::cfRate(x, phaseSpaceParams, efficiencyTimescale); };
-    auto   dcsRate             = [&](double x) { return Phys::dcsRate(x, phaseSpaceParams, efficiencyTimescale); };
+    auto cfRate  = [&](double x) { return Phys::cfRate(x, phaseSpaceParams, efficiencyTimescale); };
+    auto dcsRate = [&](double x) { return Phys::dcsRate(x, phaseSpaceParams, efficiencyTimescale); };
 
     // Generator and PDF for random numbers
     std::random_device                     rd;
@@ -95,6 +81,31 @@ void test_param_scan(void)
     SimulatedDecays MyDecays = SimulatedDecays(gen, genPDF, cfRate, dcsRate, std::make_pair(0., maxTime), _gen);
     MyDecays.findDcsDecayTimes((size_t)numDcsEvents);
     MyDecays.findCfDecayTimes(numCfEvents);
+
+    return MyDecays;
+}
+
+/*
+ * Create a dataset using accept-reject and our RatioCalculator then fit it to polynomials using
+ * Minuit ChiSq
+ *
+ * Perform a scan of each parameter, store them as std::vector<std::pair<double, double>> and plot
+ */
+void test_param_scan(void)
+{
+    double                    maxTime             = 0.004;
+    size_t                    numCfEvents         = 700000;
+    double                    efficiencyTimescale = 2500.0;
+    FitterUtil::DecayParams_t phaseSpaceParams    = {
+        .x     = WORLD_AVERAGE_X,
+        .y     = WORLD_AVERAGE_Y,
+        .r     = 0.055,
+        .z_im  = -0.2956,
+        .z_re  = 0.7609,
+        .width = efficiencyTimescale,
+    };
+
+    auto MyDecays = generateDecays(phaseSpaceParams, maxTime, numCfEvents, efficiencyTimescale);
 
     // Define some time bins
     std::vector<double> dcsTimes{MyDecays.WSDecayTimes};
@@ -171,145 +182,24 @@ void test_param_scan(void)
     delete expectedChiSq;
 }
 
-void test_2d_scan()
-{
-    // Create an accept-reject dataset
-    FitterUtil::DecayParams_t phaseSpaceParams = {
-        .x     = 0.0037,
-        .y     = 0.0066,
-        .r     = 0.055,
-        .z_im  = -0.2956,
-        .z_re  = 0.7609,
-        .width = 2439.0,
-    };
-    double maxTime = 0.002;
-
-    size_t numCfEvents  = 1000000;
-    double numDcsEvents = Phys::numDCSDecays(numCfEvents, phaseSpaceParams, maxTime, 1 / phaseSpaceParams.width);
-
-    // No efficiency
-    double efficiencyTimescale = 0;
-    auto   cfRate              = [&](double x) { return Phys::cfRate(x, phaseSpaceParams, efficiencyTimescale); };
-    auto   dcsRate             = [&](double x) { return Phys::dcsRate(x, phaseSpaceParams, efficiencyTimescale); };
-
-    // Generator and PDF for random numbers
-    std::random_device                     rd;
-    std::shared_ptr<std::mt19937>          _gen = std::make_shared<std::mt19937>(rd());
-    std::uniform_real_distribution<double> uniform;
-
-    auto gen = [&](void) {
-        double x = uniform(*_gen);
-        double z = 1 - std::exp(-1 * phaseSpaceParams.width * maxTime);
-        return (-1 / phaseSpaceParams.width) * std::log(1 - z * x);
-    };
-    auto genPDF = [&](double x) {
-        return std::exp(-phaseSpaceParams.width * x) * phaseSpaceParams.width /
-               (1 - std::exp(-phaseSpaceParams.width * maxTime));
-    };
-
-    SimulatedDecays MyDecays = SimulatedDecays(gen, genPDF, cfRate, dcsRate, std::make_pair(0., maxTime), _gen);
-    MyDecays.findDcsDecayTimes((size_t)numDcsEvents);
-    MyDecays.findCfDecayTimes(numCfEvents);
-
-    // Define some time bins
-    std::vector<double> dcsTimes{MyDecays.WSDecayTimes};
-    std::sort(dcsTimes.begin(), dcsTimes.end());
-    std::vector<double> timeBinLimits = util::findBinLimits(dcsTimes, 100, 0, 1.05 * maxTime);
-
-    // Divide using RatioCalculator
-    std::vector<size_t> cfCounts  = util::binVector(MyDecays.RSDecayTimes, timeBinLimits);
-    std::vector<size_t> dcsCounts = util::binVector(MyDecays.WSDecayTimes, timeBinLimits);
-    RatioCalculator     MyRatios  = RatioCalculator(cfCounts, dcsCounts, timeBinLimits);
-    MyRatios.calculateRatios();
-
-    // Create a fitter
-    FitData_t              MyFitData = FitData(timeBinLimits, MyRatios.ratio, MyRatios.error);
-    IntegralOptions_t      integralOptions(false, 0, timeBinLimits, 0);
-    MinuitPolynomialFitter MinuitChiSqScanner(MyFitData, integralOptions);
-
-    // Perform fit, output minimum statistic
-    std::vector<double> initialParameterGuess{0.02, 1.0, 100.0};
-    std::vector<double> initialErrorsGuess{0.01, 1.0, 100.0};
-    MinuitChiSqScanner.setPolynomialParams(initialParameterGuess, initialErrorsGuess);
-    MinuitChiSqScanner.fit();
-    std::cout << "Min chisq: " << MinuitChiSqScanner.fitParams.fitStatistic << std::endl;
-    std::cout << "Params: " << MinuitChiSqScanner.fitParams.fitParams[0] << " "
-              << MinuitChiSqScanner.fitParams.fitParams[1] << " " << MinuitChiSqScanner.fitParams.fitParams[2]
-              << std::endl;
-
-    // Perform a 2d chi squared scan on the parameters a and b
-    size_t numAPoints     = 100;
-    size_t numBPoints     = 100;
-    size_t numTotalPoints = numAPoints * numBPoints;
-    double bSigma         = 2;
-    double aSigma         = 2;
-    double minA = MinuitChiSqScanner.fitParams.fitParams[0] - aSigma * MinuitChiSqScanner.fitParams.fitParamErrors[0];
-    double minB = MinuitChiSqScanner.fitParams.fitParams[1] - bSigma * MinuitChiSqScanner.fitParams.fitParamErrors[1];
-    double maxA = MinuitChiSqScanner.fitParams.fitParams[0] + aSigma * MinuitChiSqScanner.fitParams.fitParamErrors[0];
-    double maxB = MinuitChiSqScanner.fitParams.fitParams[1] + bSigma * MinuitChiSqScanner.fitParams.fitParamErrors[1];
-
-    std::vector<std::vector<double>> twoDParameterScan = MinuitChiSqScanner.twoDParamScan(
-        0, 1, numAPoints, numBPoints, std::make_pair(minA, maxA), std::make_pair(minB, maxB));
-
-    std::vector<double> aVals(numTotalPoints);
-    std::vector<double> bVals(numTotalPoints);
-    std::vector<double> chiSquaredVals(numTotalPoints);
-
-    for (size_t i = 0; i < numTotalPoints; ++i) {
-        aVals[i]          = twoDParameterScan[i][0];
-        bVals[i]          = twoDParameterScan[i][1];
-        chiSquaredVals[i] = twoDParameterScan[i][2];
-    }
-
-    TGraph2D* Graph = new TGraph2D(numTotalPoints, aVals.data(), bVals.data(), chiSquaredVals.data());
-    Graph->SetTitle("2d a b scan;a;b;chiSq");
-    util::saveObjectToFile(Graph, "a_.pdf", "surf1");
-    delete Graph;
-    std::cout << std::endl;
-}
-
 /*
  * 2d scan of im(Z) and re(Z), both with and without a constraint
  */
 void test_z_scan()
 {
-    // Create an accept-reject dataset
-    FitterUtil::DecayParams_t phaseSpaceParams = {
+    double                    maxTime             = 0.004;
+    size_t                    numCfEvents         = 700000;
+    double                    efficiencyTimescale = 2500.0;
+    FitterUtil::DecayParams_t phaseSpaceParams    = {
         .x     = WORLD_AVERAGE_X,
         .y     = WORLD_AVERAGE_Y,
         .r     = 0.055,
-        .z_im  = -0.434,
-        .z_re  = -0.304,
-        .width = 2439.0,
-    };
-    double maxTime = 0.002;
-
-    size_t numCfEvents  = 1e7;
-    double numDcsEvents = Phys::numDCSDecays(numCfEvents, phaseSpaceParams, maxTime, 1 / phaseSpaceParams.width);
-
-    // No efficiency
-    double efficiencyTimescale = 0;
-    auto   cfRate              = [&](double x) { return Phys::cfRate(x, phaseSpaceParams, efficiencyTimescale); };
-    auto   dcsRate             = [&](double x) { return Phys::dcsRate(x, phaseSpaceParams, efficiencyTimescale); };
-
-    // Generator and PDF for random numbers
-    std::random_device                     rd;
-    std::shared_ptr<std::mt19937>          _gen = std::make_shared<std::mt19937>(rd());
-    std::uniform_real_distribution<double> uniform;
-
-    auto gen = [&](void) {
-        double x = uniform(*_gen);
-        double z = 1 - std::exp(-1 * phaseSpaceParams.width * maxTime);
-        return (-1 / phaseSpaceParams.width) * std::log(1 - z * x);
-    };
-    auto genPDF = [&](double x) {
-        return std::exp(-phaseSpaceParams.width * x) * phaseSpaceParams.width /
-               (1 - std::exp(-phaseSpaceParams.width * maxTime));
+        .z_im  = -0.2956,
+        .z_re  = 0.7609,
+        .width = efficiencyTimescale,
     };
 
-    SimulatedDecays MyDecays = SimulatedDecays(gen, genPDF, cfRate, dcsRate, std::make_pair(0., maxTime), _gen);
-    MyDecays.findDcsDecayTimes((size_t)numDcsEvents);
-    MyDecays.findCfDecayTimes(numCfEvents);
+    auto MyDecays = generateDecays(phaseSpaceParams, maxTime, numCfEvents, efficiencyTimescale);
 
     // Define some time bins
     std::vector<double> dcsTimes{MyDecays.WSDecayTimes};
@@ -345,46 +235,27 @@ void test_z_scan()
     double min            = -1;
     double max            = 1;
 
-    PhysFitter.fixParameters(std::vector<std::string>{"width", "x", "y"});
-    std::vector<std::vector<double>> twoDParameterScan =
-        PhysFitter.twoDParamScan(3, 4, numPoints, numPoints, std::make_pair(min, max), std::make_pair(min, max));
-
     PhysFitterConstraint.fixParameters(std::vector<std::string>{"width"});
     std::vector<std::vector<double>> twoDParameterScanConstraint = PhysFitterConstraint.twoDParamScan(
         3, 4, numPoints, numPoints, std::make_pair(min, max), std::make_pair(min, max));
 
     std::vector<double> imVals(numTotalPoints);
     std::vector<double> reVals(numTotalPoints);
-    std::vector<double> chiSquaredValsNoConstraint(numTotalPoints);
     std::vector<double> chiSquaredValsWithConstraint(numTotalPoints);
 
     for (size_t i = 0; i < numTotalPoints; ++i) {
-        imVals[i]                       = twoDParameterScan[i][0];
-        reVals[i]                       = twoDParameterScan[i][1];
-        chiSquaredValsNoConstraint[i]   = twoDParameterScan[i][2];
+        imVals[i]                       = twoDParameterScanConstraint[i][0];
+        reVals[i]                       = twoDParameterScanConstraint[i][1];
         chiSquaredValsWithConstraint[i] = twoDParameterScanConstraint[i][2];
     }
 
     // Subtract off the minimum chi squared
-    double minChiSqNoConstraint =
-        *std::min_element(chiSquaredValsNoConstraint.begin(), chiSquaredValsNoConstraint.end());
     double minChiSqWithConstraint =
         *std::min_element(chiSquaredValsWithConstraint.begin(), chiSquaredValsWithConstraint.end());
-    std::transform(chiSquaredValsNoConstraint.begin(),
-                   chiSquaredValsNoConstraint.end(),
-                   chiSquaredValsNoConstraint.begin(),
-                   [&](double x) { return x - minChiSqNoConstraint; });
     std::transform(chiSquaredValsWithConstraint.begin(),
                    chiSquaredValsWithConstraint.end(),
                    chiSquaredValsWithConstraint.begin(),
                    [&](double x) { return x - minChiSqWithConstraint; });
-
-    TGraph2D* NoConstraintGraph =
-        new TGraph2D(numTotalPoints, imVals.data(), reVals.data(), chiSquaredValsNoConstraint.data());
-    NoConstraintGraph->SetMaximum(25);
-    NoConstraintGraph->SetTitle("2d Z scan;Im(Z);Re(Z);chiSq");
-    util::saveObjectToFile(NoConstraintGraph, "z_.pdf", "CONT4Z");
-    delete NoConstraintGraph;
 
     // Square canvas
     TCanvas* c = new TCanvas();
@@ -478,13 +349,10 @@ void test_z_scan()
 
 int main()
 {
-    // a b c polynomial scans
-    // test_2d_scan();
-
     // r scan
     test_param_scan();
 
     // 2d scans
-    // test_z_scan();
+    test_z_scan();
     return 0;
 }
