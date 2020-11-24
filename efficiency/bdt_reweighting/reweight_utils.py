9de964caf5849bb31ef3a862269f7012b31502d2
Richard Lane <richard.lane@bristol.ac.uk>
2020-11-24 10:32:03 +0000

    fcn for finding invariant masses

--- efficiency/bdt_reweighting/reweight_utils.py
+++ efficiency/bdt_reweighting/reweight_utils.py
@@ -15,3 +15,25 @@ def read_branch(file_name: str, tree_name: str, branch_name: str) -> np.ndarray:
     tree = uproot.open(file_name)[tree_name]
 
     return tree.array(branch_name)
+
+
+def invariant_mass(particles):
+    """
+    Find the invariant mass of a collection of particles
+
+    particles should be an iterable whose data members contain .px, .py, .pz, and .energy attributes
+
+    """
+    energy = 0.0
+    px, py, pz = 0.0, 0.0, 0.0
+
+    for particle in particles:
+        energy += particle.energy
+        px += particle.px
+        py += particle.py
+        pz += particle.pz
+
+    p_squared = px ** 2 + py ** 2 + pz ** 2
+    mass_squared = energy * energy - p_squared
+
+    return np.sqrt(mass_squared)
