3660e974d9f86ecb7a31c067b4e7160800774cc8
Richard Lane <richard.lane@bristol.ac.uk>
2020-11-24 10:44:10 +0000

    fcn for plotting a hist of phsp points

--- efficiency/bdt_reweighting/reweight_utils.py
+++ efficiency/bdt_reweighting/reweight_utils.py
@@ -5,6 +5,7 @@ Python I/O, phase space parametrisation and BDT hyperparameter optimisation
 
 """
 import uproot
+import matplotlib.pyplot as plt
 
 
 def read_branch(file_name: str, tree_name: str, branch_name: str) -> np.ndarray:
@@ -21,6 +22,8 @@ def invariant_masses(px, py, pz, energy):
     """
     Find the invariant masses of a collection of particles represented by their kinematic data
 
+    all arguments are 1d iterables of momentum/energy
+
     """
     assert len(px) == len(py) == len(pz) == len(energy)
 
@@ -28,3 +31,15 @@ def invariant_masses(px, py, pz, energy):
     mass_squared = energy ** 2 - p_squared
 
     return np.sqrt(mass_squared)
+
+
+def plot_projection(phsp_points, i):
+    """
+    Plot a histogram of the i'th projection of a collection of phase space points
+
+    """
+    # Create an array of the points we're interested in
+    data = phsp_points[:, i]
+
+    plt.hist(data, bins=100)
+    plt.show()
