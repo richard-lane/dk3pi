07c9b9b958d6704fb2bb33ac2416c36494e488b3
Richard Lane <richard.lane@bristol.ac.uk>
2020-11-24 10:34:57 +0000

    inv mass fcn takes np arrays instead

--- efficiency/bdt_reweighting/reweight_utils.py
+++ efficiency/bdt_reweighting/reweight_utils.py
@@ -17,23 +17,14 @@ def read_branch(file_name: str, tree_name: str, branch_name: str) -> np.ndarray:
     return tree.array(branch_name)
 
 
-def invariant_mass(particles):
+def invariant_masses(px, py, pz, energy):
     """
-    Find the invariant mass of a collection of particles
-
-    particles should be an iterable whose data members contain .px, .py, .pz, and .energy attributes
+    Find the invariant masses of a collection of particles represented by their kinematic data
 
     """
-    energy = 0.0
-    px, py, pz = 0.0, 0.0, 0.0
-
-    for particle in particles:
-        energy += particle.energy
-        px += particle.px
-        py += particle.py
-        pz += particle.pz
+    assert len(px) == len(py) == len(pz) == len(energy)
 
     p_squared = px ** 2 + py ** 2 + pz ** 2
-    mass_squared = energy * energy - p_squared
+    mass_squared = energy ** 2 - p_squared
 
     return np.sqrt(mass_squared)
