cbbcb6fb8a8a5c54b26076459379ab056057eaff
Richard Lane <richard.lane@bristol.ac.uk>
2020-11-24 10:51:10 +0000

    import numpy

--- efficiency/bdt_reweighting/reweight_utils.py
+++ efficiency/bdt_reweighting/reweight_utils.py
@@ -5,6 +5,7 @@ Python I/O, phase space parametrisation and BDT hyperparameter optimisation
 
 """
 import uproot
+import numpy as np
 import matplotlib.pyplot as plt
 
 
@@ -43,3 +44,27 @@ def plot_projection(phsp_points, i):
 
     plt.hist(data, bins=100)
     plt.show()
+
+
+def main():
+    """
+    Test
+
+    """
+    # Read in kinematic data from a ROOT file
+    f = "wg_rs_prompt.root"
+    k_px = read_branch(f, "DecayTree", "D0_P0_PX")
+    k_py = read_branch(f, "DecayTree", "D0_P0_PY")
+    k_pz = read_branch(f, "DecayTree", "D0_P0_PZ")
+    k_e = read_branch(f, "DecayTree", "D0_P0_PE")
+
+    # Find its invariant masses
+    k_masses = invariant_masses(k_px, k_py, k_pz, k_e)
+    phsp = np.column_stack((k_masses, k_masses))
+
+    # Plot projections
+    plot_projection(phsp, 0)
+
+
+if __name__ == "__main__":
+    main()
