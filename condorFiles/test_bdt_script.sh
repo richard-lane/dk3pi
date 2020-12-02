591117544e0e304b3e15cd5d804c3e64f9031603
Richard Lane <richard.lane@bristol.ac.uk>
2020-12-02 11:44:32 +0000

    append libc64 location to LD_LIBRARY_PATH in bdt tuning script

--- condorFiles/test_bdt_script.sh
+++ condorFiles/test_bdt_script.sh
@@ -2,6 +2,9 @@
 # Check that I can run my BDT script on Condor
 
 set -e
+
+LD_LIBRARY_PATH=/usr/lib64:$LD_LIBRARY_PATH
+
 source /software/mh19137/venvs/python3/bin/activate
 which python
 locate libc.so.6
