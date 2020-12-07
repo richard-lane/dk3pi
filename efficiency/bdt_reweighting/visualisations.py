94e334b7740682501fad7da1c81f11436026b97b
Richard Lane <richard.lane@bristol.ac.uk>
2020-12-07 16:01:54 +0000

    fcn for plotting slices which kind of breaks encapsulation but whatever

--- efficiency/bdt_reweighting/visualisations.py
+++ efficiency/bdt_reweighting/visualisations.py
@@ -2,6 +2,8 @@
 Helpers for visualising our multidimensional phase space distributions
 
 """
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 class Slices:
@@ -33,15 +35,16 @@ class Slices:
         # Find the right histogram
         # If the point's _slice_index'th co-ordinate is in the N'th bin of _slice_limits,
         # it belongs in the N'th histogram slice
-        this_slice = np.digitize(point[_slice_index], _slice_limits) - 1
-        assert 0 <= this_slice < len(_slice_limits)
+        this_slice = np.digitize(point[self._slice_index], self._slice_limits) - 1
+        assert 0 <= this_slice < len(self._slice_limits)
 
         # Find the right bin in this histogram
-        this_bin = np.digitize(point[_plot_index], _bin_limits) - 1
-        assert 0 <= this_bin < len(_bin_limits)
+        this_bin = np.digitize(point[self._plot_index], self._bin_limits) - 1
+        assert 0 <= this_bin < len(self._bin_limits)
 
         # Increment the right bin
-        _slice_array[this_slice][this_bin] += weight
+        self._slice_array[this_slice][this_bin] += weight
+        self._num_points += weight
 
     def add_points(self, points, weights=None):
         """
@@ -49,19 +52,19 @@ class Slices:
 
         """
         w = weights if weights else np.ones_like(points[:, 0])
-        assert len(points) = len(w)
+        assert len(points) == len(w)
 
         for point, weight in zip(points, weights):
-            add_point(point, weight)
+            self.add_point(point, weight)
 
     # Array of histograms representing each slice through phase space
-    _slice_array = None
+    _slice_array = []
 
     # Bin limits for finding which bin a point belongs in
-    _bin_limits = None
+    _bin_limits = []
 
     # Bin limits for finding which histogram a point belongs in
-    _slice_limits = None
+    _slice_limits = []
 
     # Indices of variables that we're using to slice across, and plot projections in
     _plot_index = None
@@ -77,4 +80,26 @@ def plot_slices(path, slices, labels):
     Plot an iterable of slices to <path>0.png, <path>1.png etc.
 
     """
-    pass
+    assert len(slices) == len(labels)
+
+    num_slices = len(slices[0]._slice_array)
+    slice_limits = slices[0]._slice_limits
+    bin_limits = slices[0]._bin_limits
+    for s in slices:
+        assert len(s._slice_array) == num_slices
+        assert np.allclose(s._slice_limits, slice_limits)
+        assert np.allclose(s._bin_limits, bin_limits)
+
+    # Find the bin centres for plotting
+    centres = np.mean(np.vstack([bin_limits[0:-1], bin_limits[1:]]), axis=0)
+
+    for i in range(num_slices):
+        slice_path = path + str(i) + ".png"
+
+        for s in slices:
+            # Scale such that the sum of all slices have an area of 1
+            counts = np.copy(s._slice_array[i]) / s._num_points
+
+            # Plot
+            plt.plot(centres, counts)
+        plt.savefig(slice_path)
