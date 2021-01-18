1aafc3243143574d08f82d12e6d9e5f6a235e1e0
Richard Lane <richard.lane@bristol.ac.uk>
2021-01-18 13:50:22 +0100

    test for momentum ordering of an in-place numpy array

--- test/ut/efficiency/ut.py
+++ test/ut/efficiency/ut.py
@@ -37,6 +37,35 @@ def test_momentum_ordering():
     assert reweight_utils.momentum_order(kaon, pi2, pi1) == (pi1, pi2)
 
 
+def test_momm_ordering_array():
+    """
+    Check that momentum ordering works correctly when modifying numpy arrays in-place
+
+    """
+    # Create multidimensional numpy arrays of (k_px, k_py, k_pz, k_E) etc. for our particles
+    # Only one particle of each species so each will be a 4x1 array
+    kaon = np.array(((100,), (200,), (300,), (619.449,)))
+    pi1 = np.array(((100,), (200,), (300,), (399.349,)))  # Lower inv mass M(Kpi)
+    pi2 = np.array(((50,), (-200,), (150,), (290.654,)))  # Higher inv mass M(Kpi)
+
+    # Store copies of these arrays for comparison later
+    old_pi1, old_pi2 = pi1.copy(), pi2.copy()
+
+    # Check the no-op ordering works
+    print()
+    print(pi1,"\n", pi2)
+    pi1.T[0], pi2.T[0] = reweight_utils.momentum_order(kaon.T[0], pi1.T[0], pi2.T[0])
+    print(pi1, "\n",pi2)
+    assert np.array_equal(pi1, old_pi1)
+    assert np.array_equal(pi2, old_pi2)
+
+    # Check the ordering works
+    pi2.T[0], pi1.T[0] = reweight_utils.momentum_order(kaon.T[0], pi1.T[0], pi2.T[0])
+    print(pi1, "\n",pi2)
+    assert np.array_equal(pi1, old_pi1)
+    assert np.array_equal(pi2, old_pi2)
+
+
 def test_inv_mass_consistency():
     """
     Consistency check that the invariant mass fcn using a single particle returns the same result as when using a numpy array
