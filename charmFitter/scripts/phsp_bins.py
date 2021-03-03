eab80d9ac45a83922fb7e8ec5de1e155b2ad7f3c
Richard Lane <richard.lane@bristol.ac.uk>
2021-03-03 15:32:59 +0000

    script for visualising dalitz plot projections in phsp bins

--- charmFitter/scripts/phsp_bins.py
+++ charmFitter/scripts/phsp_bins.py
@@ -24,6 +24,7 @@ sys.path.append(
 )
 import amplitude_models
 import script_util
+import reweight_utils
 
 
 def _polar_plot(magnitudes, phases):
@@ -100,9 +101,70 @@ def _hist(phases, title):
     plt.savefig(title.replace(" ", "_") + ".png", bbox_inches="tight")
 
 
+def _binned_invmasses(events, phases, *particles):
+    """
+    Find the invariant masses of a number of particles
+
+    particles should be any of 0,1,2,3
+
+    """
+    bin_limits = (-180.0, -39.0, 0.0, 43.0, 180.0)
+    x = [[] for _ in range(4)]
+
+    for evt, phase in zip(events.T, phases):
+        px = 0.0
+        py = 0.0
+        pz = 0.0
+        e = 0.0
+        for particle in particles:
+            px += evt[0 + particle * 4]
+            py += evt[1 + particle * 4]
+            pz += evt[2 + particle * 4]
+            e += evt[3 + particle * 4]
+
+        s = reweight_utils.invariant_mass(px, py, pz, e)
+
+        # Append to the right list based on phase
+        for i in range(len(bin_limits) - 1):
+            if bin_limits[i] < phase <= bin_limits[i + 1]:
+                x[i].append(s)
+
+    return x
+
+
+def _dalitz(
+    rs, ws, flat, rs_phases, ws_phases, flat_phases, title, x_indices, y_indices
+):
+    """
+    Plot a scatter plot of points, colour coded by phsp bin
+
+    finds invariant masses s_ij based on ij... in x_indices, y_indices
+
+    """
+    # Plot
+    fig, ax = plt.subplots(1, 3)
+
+    for a in ax:
+        a.set_xlabel(r"$M(K\pi_1)$")
+        a.grid(False)
+        a.set_xticklabels([])
+        a.set_yticklabels([])
+
+    ax[0].set_ylabel(r"$M(K\pi_2\pi_3)$")
+
+    for i, (data, phase) in enumerate(
+        zip((rs, ws, flat), (rs_phases, ws_phases, flat_phases))
+    ):
+        binned_data_x = _binned_invmasses(data, phase, *x_indices)
+        binned_data_y = _binned_invmasses(data, phase, *y_indices)
+        for (dx, dy) in zip(binned_data_x, binned_data_y):
+            ax[i].plot(dx, dy, ".")
+
+    fig.suptitle(title)
+    plt.show()
+
+
 def _read(file_name):
-    """
-    """
     tree = uproot.open(file_name)["DalitzEventList"]
     return np.row_stack(
         (
@@ -126,21 +188,87 @@ def _read(file_name):
     )
 
 
+def _all_two_body_projections(rs, ws, flat, rs_phases, ws_phases, flat_phases):
+    # lol
+    for a in range(4):
+        for b in range(4):
+            for c in range(4):
+                for d in range(4):
+                    # Plot only interesting ones
+                    if a != b and c != d:
+                        if set((a, b)) != set((c, d)):
+                            _dalitz(
+                                rs,
+                                ws,
+                                flat,
+                                rs_phases,
+                                ws_phases,
+                                flat_phases,
+                                r"$s_{"
+                                + str(a)
+                                + str(b)
+                                + r"} vs s_{"
+                                + str(c)
+                                + str(d)
+                                + r"}$",
+                                (a, b),
+                                (c, d),
+                            )
+
+
+def _simple_binning_plot(data):
+    x = [[] for _ in range(4)]
+    y = [[] for _ in range(4)]
+
+    lims = 1.0, 1.4
+
+    # Find inv masses
+    for evt in data.T:
+        s01 = reweight_utils.invariant_mass(
+            evt[0] + evt[4], evt[1] + evt[5], evt[2] + evt[6], evt[3] + evt[7]
+        )
+        s023 = reweight_utils.invariant_mass(
+            evt[0] + evt[8] + evt[12],
+            evt[1] + evt[9] + evt[13],
+            evt[2] + evt[10] + evt[14],
+            evt[3] + evt[11] + evt[15],
+        )
+        bin = 0
+        if s01 < lims[0] and s023 > lims[1]:
+            bin=1
+        if s01 > lims[0] and s023 < lims[1]:
+            bin=2
+        if s01 > lims[0] and s023 > lims[1]:
+            bin=3
+
+        x[bin].append(s01)
+        y[bin].append(s023)
+
+    # Plot
+    for i in range(4):
+        plt.plot(x[i], y[i], ".")
+
+    plt.xlabel(r"$M(K\pi_1) /GeV$")
+    plt.ylabel(r"$M(K\pi_2\pi_3) /GeV$")
+    plt.savefig("phsp.png", bbox_inches="tight")
+
+
 def main():
     # Read in RS, WS, flat data (use ampgen)
-    rs = _read("Favoured.root")
-    phases = [np.rad2deg(amplitude_models.phase(evt, 1)) for evt in rs.T]
-    _hist(phases, "AmpGen RS phases")
+    #rs = _read("shortFavoured.root")
+    #rs_phases = [np.rad2deg(amplitude_models.phase(evt, 1)) for evt in rs.T]
 
-    ws = _read("Mixed.root")
-    phases = [np.rad2deg(amplitude_models.phase(evt, 1)) for evt in ws.T]
-    _hist(phases, "AmpGen WS phases")
+    #ws = _read("shortMixed.root")
+    #ws_phases = [np.rad2deg(amplitude_models.phase(evt, 1)) for evt in ws.T]
 
-    flat = np.row_stack(script_util.flat_phsp_points(1000000)) / 1000.0
-    phases = [np.rad2deg(amplitude_models.phase(evt, 1)) for evt in flat.T]
-    _hist(phases, "Phsp Evts phases")
+    flat = np.row_stack(script_util.flat_phsp_points(3000)) / 1000.0
+    print(flat)
+    #flat_phases = [np.rad2deg(amplitude_models.phase(evt, 1)) for evt in flat.T]
 
-    # _polar_plot(magnitudes, phases)
+    _simple_binning_plot(flat)
+
+    # Two body dalitz plots
+    # _all_two_body_projections(rs, ws, flat, rs_phases, ws_phases, flat_phases)
 
 
 if __name__ == "__main__":
