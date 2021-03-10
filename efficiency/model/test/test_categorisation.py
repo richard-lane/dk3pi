817b7334664a64668ac63492369fcaf91f4c96b8
Richard Lane <richard.lane@bristol.ac.uk>
2021-03-10 17:35:12 +0000

    test for time binning

--- efficiency/model/test/test_categorisation.py
+++ efficiency/model/test/test_categorisation.py
@@ -10,3 +10,8 @@ def test_time_bin_underflow():
 def test_time_bin_overflow():
     with pytest.raises(ValueError):
         categorisation.time_bin(0.041)
+
+
+def test_time_bin():
+    time = 0.00041  # 1 D lifetime; should go in bin 2
+    assert categorisation.time_bin(time) == 2
