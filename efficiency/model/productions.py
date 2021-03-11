9eebb725e5f71420428322b4e4aa0c6f38f59f64
Richard Lane <richard.lane@bristol.ac.uk>
2021-03-11 14:16:34 +0000

    accurate locations of wg prods relative to lxplus root

--- efficiency/model/productions.py
+++ efficiency/model/productions.py
@@ -9,38 +9,38 @@ production = {
     },
     2016: {
         "MagUp": {
-            27165070: "root://eoslhcb.cern.ch//eos/lhcb/grid/prod/lhcb/MC/2016/CHARM_D02HHHH_DVNTUPLE.ROOT/00123278/0000/*.root",
-            27165071: "root://eoslhcb.cern.ch//eos/lhcb/grid/prod/lhcb/MC/2016/CHARM_D02HHHH_DVNTUPLE.ROOT/00123280/0000/*.root",
-            27165072: "root://eoslhcb.cern.ch//eos/lhcb/grid/prod/lhcb/MC/2016/CHARM_D02HHHH_DVNTUPLE.ROOT/00123276/0000/*.root",
+            27165070: "/eos/lhcb/grid/prod/lhcb/MC/2016/CHARM_D02HHHH_DVNTUPLE.ROOT/00123278/0000/*.root",
+            27165071: "/eos/lhcb/grid/prod/lhcb/MC/2016/CHARM_D02HHHH_DVNTUPLE.ROOT/00123280/0000/*.root",
+            27165072: "/eos/lhcb/grid/prod/lhcb/MC/2016/CHARM_D02HHHH_DVNTUPLE.ROOT/00123276/0000/*.root",
         },
         "MagDown": {
-            27165070: "root://eoslhcb.cern.ch//eos/lhcb/grid/prod/lhcb/MC/2016/CHARM_D02HHHH_DVNTUPLE.ROOT/00123316/0000/*.root",
-            27165071: "root://eoslhcb.cern.ch//eos/lhcb/grid/prod/lhcb/MC/2016/CHARM_D02HHHH_DVNTUPLE.ROOT/00123314/0000/*.root",
-            27165072: "root://eoslhcb.cern.ch//eos/lhcb/grid/prod/lhcb/MC/2016/CHARM_D02HHHH_DVNTUPLE.ROOT/00123296/0000/*.root",
+            27165070: "/eos/lhcb/grid/prod/lhcb/MC/2016/CHARM_D02HHHH_DVNTUPLE.ROOT/00123316/0000/*.root",
+            27165071: "/eos/lhcb/grid/prod/lhcb/MC/2016/CHARM_D02HHHH_DVNTUPLE.ROOT/00123314/0000/*.root",
+            27165072: "/eos/lhcb/grid/prod/lhcb/MC/2016/CHARM_D02HHHH_DVNTUPLE.ROOT/00123296/0000/*.root",
         },
     },
     2017: {
         "MagUp": {
-            27165070: "root://eoslhcb.cern.ch//eos/lhcb/grid/prod/lhcb/MC/2017/CHARM_D02HHHH_DVNTUPLE.ROOT/00123308/0000/*.root",
-            27165071: "root://eoslhcb.cern.ch//eos/lhcb/grid/prod/lhcb/MC/2017/CHARM_D02HHHH_DVNTUPLE.ROOT/00123306/0000/*.root",
-            27165072: "root://eoslhcb.cern.ch//eos/lhcb/grid/prod/lhcb/MC/2017/CHARM_D02HHHH_DVNTUPLE.ROOT/00123310/0000/*.root",
+            27165070: "/eos/lhcb/grid/prod/lhcb/MC/2017/CHARM_D02HHHH_DVNTUPLE.ROOT/00123308/0000/*.root",
+            27165071: "/eos/lhcb/grid/prod/lhcb/MC/2017/CHARM_D02HHHH_DVNTUPLE.ROOT/00123306/0000/*.root",
+            27165072: "/eos/lhcb/grid/prod/lhcb/MC/2017/CHARM_D02HHHH_DVNTUPLE.ROOT/00123310/0000/*.root",
         },
         "MagDown": {
-            27165070: "root://eoslhcb.cern.ch//eos/lhcb/grid/prod/lhcb/MC/2017/CHARM_D02HHHH_DVNTUPLE.ROOT/00123318/0000/*.root",
-            27165071: "root://eoslhcb.cern.ch//eos/lhcb/grid/prod/lhcb/MC/2017/CHARM_D02HHHH_DVNTUPLE.ROOT/00123320/0000/*.root",
-            27165072: "root://eoslhcb.cern.ch//eos/lhcb/grid/prod/lhcb/MC/2017/CHARM_D02HHHH_DVNTUPLE.ROOT/00123322/0000/*.root",
+            27165070: "/eos/lhcb/grid/prod/lhcb/MC/2017/CHARM_D02HHHH_DVNTUPLE.ROOT/00123318/0000/*.root",
+            27165071: "/eos/lhcb/grid/prod/lhcb/MC/2017/CHARM_D02HHHH_DVNTUPLE.ROOT/00123320/0000/*.root",
+            27165072: "/eos/lhcb/grid/prod/lhcb/MC/2017/CHARM_D02HHHH_DVNTUPLE.ROOT/00123322/0000/*.root",
         },
     },
     2018: {
         "MagUp": {
-            27165070: "root://eoslhcb.cern.ch//eos/lhcb/grid/prod/lhcb/MC/2018/CHARM_D02HHHH_DVNTUPLE.ROOT/00123286/0000/*.root",
-            27165071: "root://eoslhcb.cern.ch//eos/lhcb/grid/prod/lhcb/MC/2018/CHARM_D02HHHH_DVNTUPLE.ROOT/00123288/0000/*.root",
-            27165072: "root://eoslhcb.cern.ch//eos/lhcb/grid/prod/lhcb/MC/2018/CHARM_D02HHHH_DVNTUPLE.ROOT/00123290/0000/*.root",
+            27165070: "/eos/lhcb/grid/prod/lhcb/MC/2018/CHARM_D02HHHH_DVNTUPLE.ROOT/00123286/0000/*.root",
+            27165071: "/eos/lhcb/grid/prod/lhcb/MC/2018/CHARM_D02HHHH_DVNTUPLE.ROOT/00123288/0000/*.root",
+            27165072: "/eos/lhcb/grid/prod/lhcb/MC/2018/CHARM_D02HHHH_DVNTUPLE.ROOT/00123290/0000/*.root",
         },
         "MagDown": {
-            27165070: "root://eoslhcb.cern.ch//eos/lhcb/grid/prod/lhcb/MC/2018/CHARM_D02HHHH_DVNTUPLE.ROOT/00123274/0000/*.root",
-            27165071: "root://eoslhcb.cern.ch//eos/lhcb/grid/prod/lhcb/MC/2018/CHARM_D02HHHH_DVNTUPLE.ROOT/00123272/0000/*.root",
-            27165072: "root://eoslhcb.cern.ch//eos/lhcb/grid/prod/lhcb/MC/2018/CHARM_D02HHHH_DVNTUPLE.ROOT/00123270/0000/*.root",
+            27165070: "/eos/lhcb/grid/prod/lhcb/MC/2018/CHARM_D02HHHH_DVNTUPLE.ROOT/00123274/0000/*.root",
+            27165071: "/eos/lhcb/grid/prod/lhcb/MC/2018/CHARM_D02HHHH_DVNTUPLE.ROOT/00123272/0000/*.root",
+            27165072: "/eos/lhcb/grid/prod/lhcb/MC/2018/CHARM_D02HHHH_DVNTUPLE.ROOT/00123270/0000/*.root",
         },
     },
 }
