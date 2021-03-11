66f44035ee5c7aefc4555946a8814fbad48b75d1
Richard Lane <richard.lane@bristol.ac.uk>
2021-03-11 11:30:37 +0000

    python DCS and CF fcns

--- efficiency/model/categorisation.py
+++ efficiency/model/categorisation.py
@@ -38,12 +38,13 @@ VETO_WIDTH = 0.010
 OFFSET_MAG = 0.0601387
 OFFSET_PHASE = 1.04827  # degrees
 DCS_OFFSET = OFFSET_MAG * np.exp((0 + 1j) * OFFSET_PHASE * np.pi / 180.0)
+
 MODEL_DIR = os.path.join(os.path.dirname(__file__), "amplitude_models")
+CF_LIB = os.path.abspath(os.path.join(MODEL_DIR, "cf_wrapper.so"))
+DCS_LIB = os.path.abspath(os.path.join(MODEL_DIR, "dcs_wrapper.so"))
 
 # If our shared libraries haven't been built, build them
-if not os.path.exists(
-    os.path.abspath(os.path.join(MODEL_DIR, "cf_wrapper.so"))
-) or not os.path.exists(os.path.abspath(os.path.join(MODEL_DIR, "dcs_wrapper.so"))):
+if not os.path.exists(CF_LIB) or not os.path.exists(DCS_LIB):
     build_script = os.path.join(MODEL_DIR, "build.sh")
     print(
         f"Building AmpGen wrapper libs, required from \n\t{__file__} ...",
@@ -54,6 +55,85 @@ if not os.path.exists(
         [build_script], stdout=subprocess.PIPE, stderr=subprocess.PIPE, check=True
     )
     print("done")
+
+
+class Complex(ctypes.Structure):
+    """
+    Class that enables us to read a simple struct returned from C
+
+    """
+
+    _fields_ = [("real", ctypes.c_double), ("imag", ctypes.c_double)]
+
+
+def _find_fcn(lib_name, fcn_name):
+    """
+    Find the named function in a shared library
+
+    This will look in the shared library for the function of the provided name, and enforce that its
+    argument types are an array and an integer, and that it returns a Complex
+
+    :param lib_name: Location of the shared library to look in
+    :param fcn_name: Name of the function to return
+    :return        : A ctypes handle to the function
+
+    """
+    dll = ctypes.cdll.LoadLibrary(lib_name)
+    fcn = getattr(dll, fcn_name)
+    fcn.argtypes = [ctypes.POINTER(ctypes.c_double), ctypes.c_int]
+    fcn.restype = Complex
+
+    return fcn
+
+
+def _amplitude(fcn, event, k_charge):
+    """
+    Find an amplitude from an event and amplitude fcn
+
+    """
+    # Need to copy the event otherwise our array may not be contiguous, and so can't be read by ctypes
+    event = np.copy(event)
+
+    # Convert our arguments to compatible types
+    event_p = event.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
+    k_charge_ctype = ctypes.c_int(k_charge)
+
+    # Call the function
+    return fcn(event_p, k_charge_ctype)
+
+
+def cf_amplitude(event: np.ndarray, k_charge: int):
+    """
+    Find the CF amplitude of an event
+
+    :param event   : a 16-element array of kinematic parameters (kpx, kpy, kpz, kE, ...) for k, pi1, pi2, pi3
+    :param k_charge: the charge of the kaon, i.e. +1 or -1
+    :raises        : many things
+    :returns       : the complex amplitude as an instance of the builtin complex
+
+    """
+    # Find the function that we need in our shared library
+    cf_fcn = _find_fcn(CF_LIB, "cf_wrapper")
+
+    retval = _amplitude(cf_fcn, event, k_charge)
+    return complex(retval.real, retval.imag)
+
+
+def dcs_amplitude(event: np.ndarray, k_charge: int):
+    """
+    Find the DCS amplitude of an event
+
+    :param event   : a 16-element array of kinematic parameters (kpx, kpy, kpz, kE, ...) for k, pi1, pi2, pi3
+    :param k_charge: the charge of the kaon, i.e. +1 or -1
+    :raises        : many things
+    :returns       : the complex amplitude as an instance of the builtin complex
+
+    """
+    # Find the function that we need in our shared library
+    dcs_fcn = _find_fcn(DCS_LIB)
+
+    retval = _amplitude(dcs_fcn, event, k_charge)
+    return complex(retval.real, retval.imag)
 
 
 def time_bin(time):
