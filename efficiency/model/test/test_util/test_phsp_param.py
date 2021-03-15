0370f0bd964c32c252180a04af0598c195e2f3a1
Richard Lane <richard.lane@bristol.ac.uk>
2021-03-15 17:02:49 +0000

    tests for momm ordering

--- efficiency/model/test/test_util/test_phsp_param.py
+++ efficiency/model/test/test_util/test_phsp_param.py
@@ -32,3 +32,48 @@ def test_inv_masses():
         phsp_parameterisation.invariant_masses(*particles), (k_mass, pi_mass, pi_mass)
     )
 
+
+def test_momentum_ordering():
+    """
+    Check that pions are returned in the correct order when passed to momentum_order
+
+    """
+    kaon = [100, 200, 300, 619.449]
+    pi1 = [100, 200, 300, 399.349]  # Lower inv mass M(Kpi)
+    pi2 = [50, -200, 150, 290.654]  # Higher inv mass M(Kpi)
+
+    assert np.array_equal(
+        phsp_parameterisation.momentum_order(kaon, pi1, pi2), (pi1, pi2)
+    )
+    assert np.array_equal(
+        phsp_parameterisation.momentum_order(kaon, pi2, pi1), (pi1, pi2)
+    )
+
+
+def test_momm_ordering_array():
+    """
+    Check that momentum ordering works correctly when modifying numpy arrays in-place
+
+    """
+    # Create multidimensional numpy arrays of (k_px, k_py, k_pz, k_E) etc. for our particles
+    # Only one particle of each species so each will be a 4x1 array
+    kaon = np.array(((100,), (200,), (300,), (619.449,)))
+    pi1 = np.array(((100,), (200,), (300,), (399.349,)))  # Lower inv mass M(Kpi)
+    pi2 = np.array(((50,), (-200,), (150,), (290.654,)))  # Higher inv mass M(Kpi)
+
+    # Store copies of these arrays for comparison later
+    old_pi1, old_pi2 = pi1.copy(), pi2.copy()
+
+    # Check the no-op ordering works
+    pi1.T[0], pi2.T[0] = phsp_parameterisation.momentum_order(
+        kaon.T[0], pi1.T[0], pi2.T[0]
+    )
+    assert np.array_equal(pi1, old_pi1)
+    assert np.array_equal(pi2, old_pi2)
+
+    # Check the ordering works
+    pi2.T[0], pi1.T[0] = phsp_parameterisation.momentum_order(
+        kaon.T[0], pi1.T[0], pi2.T[0]
+    )
+    assert np.array_equal(pi2, old_pi1)
+    assert np.array_equal(pi1, old_pi2)
