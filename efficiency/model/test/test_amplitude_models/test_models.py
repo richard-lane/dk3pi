45e5aae1bc06cb65dabf67d7f63fae327acfbfd3
Richard Lane <richard.lane@bristol.ac.uk>
2021-03-24 16:55:15 +0000

    skel for CF test

---
+++ efficiency/model/test/test_amplitude_models/test_models.py
@@ -0,0 +1,13 @@
+import phasespace
+
+
+def test_cf():
+    """
+    Test CF model for consistency with AmpGen
+
+    """
+    # Generate phsp events
+    # Weight according to CF amplitude model
+    # Read AmpGen events
+    # Plot projections of them both to see what they look like
+    ...
