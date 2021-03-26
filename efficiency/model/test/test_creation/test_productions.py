aa9a8a462d58bd273ed05928fc31ca5f44cec9ea
Richard Lane <richard.lane@bristol.ac.uk>
2021-03-26 19:54:14 +0100

    ut for production getter

---
+++ efficiency/model/test/test_creation/test_productions.py
@@ -0,0 +1,34 @@
+from creation import productions
+import pytest
+
+
+def test_get_production():
+    """
+    Test that we can uniquely identify an existing analysis production using decay type, year and magnetisation
+
+    """
+    assert productions.get("MagDown", 2018, "RS")
+
+
+def test_get_nonexistent_production():
+    """
+    Test that attempting to get a nonexistent production throws a KeyError
+
+    """
+    with pytest.raises(KeyError):
+        productions.get("MagDown", 2015, "RS")
+
+
+def test_get_production_bad_args():
+    """
+    Test that attempting to get a production with bad args throws an AssertionError
+
+    """
+    with pytest.raises(AssertionError):
+        productions.get("MagSideways", 2018, "RS")
+
+    with pytest.raises(AssertionError):
+        productions.get("MagDown", "Tuesday", "RS")
+
+    with pytest.raises(AssertionError):
+        productions.get("MagDown", 2018, "no")
