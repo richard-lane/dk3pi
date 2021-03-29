ce20e0ca0d81b1ddc405b04ab20690945d1feaa0
Richard Lane <richard.lane@bristol.ac.uk>
2021-03-29 16:41:43 +0200

    ks veto

--- efficiency/create.py
+++ efficiency/create.py
@@ -22,25 +22,25 @@ def main():
         # Find array of phsp points
         k = np.array(
             [
-                tree[branch].array()
+                0.001 * tree[branch].array()
                 for branch in ("D0_P0_PX", "D0_P0_PY", "D0_P0_PZ", "D0_P0_PE")
             ]
         )
         pi1 = np.array(
             [
-                tree[branch].array()
+                0.001 * tree[branch].array()
                 for branch in ("D0_P1_PX", "D0_P1_PY", "D0_P1_PZ", "D0_P1_PE")
             ]
         )
         pi2 = np.array(
             [
-                tree[branch].array()
+                0.001 * tree[branch].array()
                 for branch in ("D0_P2_PX", "D0_P2_PY", "D0_P2_PZ", "D0_P2_PE")
             ]
         )
         pi3 = np.array(
             [
-                tree[branch].array()
+                0.001 * tree[branch].array()
                 for branch in ("D0_P3_PX", "D0_P3_PY", "D0_P3_PZ", "D0_P3_PE")
             ]
         )
@@ -60,6 +60,14 @@ def main():
             axis=0,
         )
 
+        pi1pi2_masses = phsp_parameterisation.invariant_masses(*(np.add(pi1, pi2)))
+        pi2pi3_masses = phsp_parameterisation.invariant_masses(*(np.add(pi2, pi3)))
+        mask = np.logical_and(
+            abs(pi1pi2_masses - definitions.KS_MASS) < definitions.VETO_WIDTH,
+            abs(pi2pi3_masses - definitions.KS_MASS) < definitions.VETO_WIDTH,
+        )
+        phsp_points = np.delete(phsp_points, np.where(mask), axis=0)
+
         # Find array of decay times
         times = np.concatenate((times, tree["D0_TAU"].array()))
 
@@ -71,29 +79,29 @@ def main():
 
     with uproot.open(definitions.RS_AMPGEN_PATH) as f:
         tree = f["DalitzEventList"]
-        ag_times = ag_times, tree["Dbar0_decayTime"].array()
+        ag_times = tree["Dbar0_decayTime"].array()
 
         k = np.array(
             [
-                1000 * tree[branch].array()
+                tree[branch].array()
                 for branch in ("_1_K~_Px", "_1_K~_Py", "_1_K~_Pz", "_1_K~_E")
             ]
         )
         pi1 = np.array(
             [
-                1000 * tree[branch].array()
+                tree[branch].array()
                 for branch in ("_2_pi#_Px", "_2_pi#_Py", "_2_pi#_Pz", "_2_pi#_E")
             ]
         )
         pi2 = np.array(
             [
-                1000 * tree[branch].array()
+                tree[branch].array()
                 for branch in ("_3_pi#_Px", "_3_pi#_Py", "_3_pi#_Pz", "_3_pi#_E")
             ]
         )
         pi3 = np.array(
             [
-                1000 * tree[branch].array()
+                tree[branch].array()
                 for branch in ("_4_pi~_Px", "_4_pi~_Py", "_4_pi~_Pz", "_4_pi~_E")
             ]
         )
@@ -105,7 +113,15 @@ def main():
 
         ag_phsp = phsp_parameterisation.invariant_mass_parametrisation(k, pi1, pi2, pi3)
 
-    # Perform Ks veto
+        # Perform Ks veto
+        # Could speed this up
+        pi1pi2_masses = phsp_parameterisation.invariant_masses(*(np.add(pi1, pi2)))
+        pi2pi3_masses = phsp_parameterisation.invariant_masses(*(np.add(pi2, pi3)))
+        mask = np.logical_and(
+            abs(pi1pi2_masses - definitions.KS_MASS) < definitions.VETO_WIDTH,
+            abs(pi2pi3_masses - definitions.KS_MASS) < definitions.VETO_WIDTH,
+        )
+        ag_phsp = np.delete(ag_phsp, np.where(mask), axis=0)
 
     d_lifetime = 4.101e-4
     times /= d_lifetime
@@ -116,10 +132,18 @@ def main():
     kw = {
         "bins": 100,
         "alpha": 0.3,
-        "density": True,
+        "density": False,
     }
-    plt.hist(phsp_points[:, 1], **kw)
-    plt.hist(ag_phsp[:, 1], **kw)
+    plt.hist(phsp_points[:, 2], **kw)
+    plt.hist(ag_phsp[:, 2], **kw)
+    plt.hist(
+        np.linspace(
+            definitions.KS_MASS - definitions.VETO_WIDTH,
+            definitions.KS_MASS + definitions.VETO_WIDTH,
+            100000,
+        ),
+        **kw
+    )
 
     plt.savefig("tmp.png")
 
