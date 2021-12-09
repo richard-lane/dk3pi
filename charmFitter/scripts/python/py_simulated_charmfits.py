7db1023feaf4ce123724898ab0d813a4f7875266
Richard Lane <richard.lane@bristol.ac.uk>
2021-12-09 21:23:43 +0100

    Squashed commit of the following:
    commit 670be9d92958eb6f426c118c5cc91c35cba3c71a
    Author: Richard Lane <richard.lane@bristol.ac.uk>
    Date:   Thu Dec 9 21:23:25 2021 +0100
    
        remove the old scripts
    
    commit f46dd9db5302e2d0b1e2d3b61facc71fa1201e14
    Author: Richard Lane <richard.lane@bristol.ac.uk>
    Date:   Thu Dec 9 21:22:41 2021 +0100
    
        plot all of them on the same axes

--- charmFitter/scripts/python/py_simulated_charmfits.py
+++ charmFitter/scripts/python/py_simulated_charmfits.py
@@ -6,24 +6,35 @@ Do a check for charm fitter scan using the python binding
 import numpy as np
 import sys, os
 import matplotlib.pyplot as plt
+from matplotlib.patches import Circle
 
 sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))), "python"))
 sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))), "build/charmFitter/python"))
-print(sys.path)
 
 import cleoScan
 import libcleoScan
 import common
 
 
-def main():
-    # Simulate decays
-    # Define our parameters
+def _normalise(l: np.ndarray):
+    """
+    Arrays are passed by reference, effectively
+
+    """
+    # Subtract off minimum
+    l[l != 0] -= np.min(l[l != 0])
+
+    # Take sqrt to convert to sigmas
+    return np.sqrt(l)
+
+
+def main(phsp_bin):
+    # Define parameters used for simulating decays
     width = 2500.0
     decay_params = [0.0039183, 0.0065139, 0.055, 0.5, 0.0, width]
 
     max_time = 0.004
-    n_rs = 10_000_000  # Number of RS evts to generate each time
+    n_rs = 10_000_000  # Number of RS evts to generate
     bins = [0, 0.00003, 0.00008, 0.00015, 0.00020, 0.00030, 0.00045, 0.00058, 0.00060, 0.00085, 0.0012, 0.004]
 
     # Simulate decays
@@ -31,36 +42,67 @@ def main():
     rs_t, ws_t = libcleoScan.simulate(n_rs, decay_params, max_time, seed)
 
     # Set up params
-    n_re_z, n_im_z = 25, 25
-    reZVals = np.linspace(-1.0, 1.0, n_re_z)
-    imZVals = np.linspace(-1.0, 1.0, n_im_z)
-    rsWeights    = np.ones(len(rs_t))
+    n_re_z, n_im_z = 50, 50
+    reZVals = np.linspace(-0.99, 0.99, n_re_z)
+    imZVals = np.linspace(-0.99, 0.99, n_im_z)
+    rsWeights = np.ones(len(rs_t))
     wsWeights    = np.ones(len(ws_t))
     initialErrs  = [1, 1, 1, 1, 1, 1]
 
     # Find chisqs
-    l = cleoScan.charmScan(reZVals,
-             imZVals,
-             rs_t,
-             rsWeights,
-             ws_t,
-             wsWeights,
-             bins,
-             decay_params,
-             initialErrs)
+    charm_likelihood = _normalise(cleoScan.charmScan(reZVals,
+                                          imZVals,
+                                          rs_t,
+                                          rsWeights,
+                                          ws_t,
+                                          wsWeights,
+                                          bins,
+                                          decay_params,
+                                          initialErrs))
 
-    # Subtract off minimum
-    l -= np.min(l)
+    cleo_likelihood = cleoScan.cleoLikelihoods(reZVals, imZVals, decay_params, phsp_bin)
 
-    # Take sqrt to convert to sigmas
-    l = np.sqrt(l)
+    # Find the min non-NaN value, replace NaNs with it
+    cleo_max = np.min(cleo_likelihood[~np.isnan(cleo_likelihood)])  # Min instead of max because we're dealing with -2LL
+    cleo_likelihood[np.isnan(cleo_likelihood)] = cleo_max
+    cleo_likelihood = _normalise(-2.0 * cleo_likelihood)
+
+    combined_likelihood = _normalise(cleoScan.cleoScan(reZVals,
+                                            imZVals,
+                                            rs_t,
+                                            rsWeights,
+                                            ws_t,
+                                            wsWeights,
+                                            bins,
+                                            decay_params,
+                                            initialErrs,
+                                            phsp_bin))
 
     # Plot something
-    plt.contourf(reZVals, imZVals, l, 10)
-    plt.colorbar()
-    plt.savefig("py_charm_scan.png")
+    titles = "Charm Mixing", "CLEO", "Both"
+    fig, ax = plt.subplots(1, 3, figsize=(12, 4), sharey=True)
+    for a, l, t in zip(ax, (charm_likelihood, cleo_likelihood, combined_likelihood), titles):
+        contours = a.contourf(reZVals, imZVals, l, list(range(4)), cmap="turbo")
+        a.add_patch(Circle((0, 0), 1.0, color="k", linestyle="--", fill=False))
+        a.set_title(t)
+        a.set_xlabel(r"Re(Z)")
+        a.set_ylabel(r"Im(Z)")
+
+    fig.subplots_adjust(right=0.8)
+    cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
+    cbar = fig.colorbar(contours, cax=cbar_ax)
+    cbar.ax.set_ylabel(r"$\sigma$", rotation=90)
+
+    fig.suptitle("$Phsp bin" + str(phsp_bin) + " : Z_\Omega^{K3\pi} Constraint$")
+
+    plt.savefig(f"py_scans_bin{phsp_bin}.png")
 
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) == 1:
+        phsp_bin = 0
+    else:
+        phsp_bin = int(sys.argv[1])
+
+    main(phsp_bin)
 
